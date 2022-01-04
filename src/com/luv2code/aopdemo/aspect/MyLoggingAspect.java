a6ee6d272f102029e58d9d8a9333de6c614b0467
unsmoker <hasangaly@gmail.com>
2022-01-04 12:40:49 -0600

    Add Pointcut Expression examples

--- src/com/luv2code/aopdemo/aspect/MyLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyLoggingAspect.java
@@ -7,7 +7,25 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class MyLoggingAspect {
-    @Before("execution(public void addAccount())")
+    /**
+     * Pointcut Expression Examples
+     *
+     * specific to AccountDAO method
+     *  @Before("execution (public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+     *
+     * Any class and any method starts with add
+     * @Before("execution (public void *.add*())")
+     *
+     * Match addAccount method that have Account param
+     * @Before("execution (* addAccount(com.luv2code.aopdemo.Account))
+     *
+     * Match addAccount method with any type and any param count
+     * @Before("execution (* addAccount(..))")
+     */
+//      @Before("execution(public void addAccount())")
+//    @Before("execution (public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+//    @Before("execution (public void *.add*())")
+    @Before("execution (* addAccount(..))")
     public void addAccountLogging(){
         System.out.println("===>Logging before execution method addAccount");
     }
